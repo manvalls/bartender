879fdb4b95bcadc92d97804c36298cc288405bc7
Manuel Valls Fernández <me@manvalls.com>
2022-10-26 21:16:56 +0000

    Add order list

--- dashboard/src/components/OrderList.js
+++ dashboard/src/components/OrderList.js
@@ -1,4 +1,32 @@
+import { useEffect, useState } from 'react'
+import { getOrders } from '../utils/api.js'
 
-const OrderList = () => null
+const OrderList = () => {
+  const [orders, setOrders] = useState([])
+  
+  const fetchOrders = async () => {
+    const orders = await getOrders()
+    setOrders(orders)
+  }
+
+  useEffect(() => {
+    fetchOrders()
+    return () => {} 
+  }, [])
+
+  return (
+    <>
+      <h2>Orders</h2>
+      <ul>
+        {orders.map((order) => (
+          <li key={order.time}>
+            {order.customer} ordered {order.type}
+          </li>
+        ))}
+      </ul>
+      <button onClick={fetchOrders}>Refresh</button>
+    </>
+  )
+}
 
 export default OrderList
